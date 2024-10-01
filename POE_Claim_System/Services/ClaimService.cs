235b7f8b4d11d2adf2645e6eb0b89b4fcaa4e4f3
Zoe-Hey <150931346+Zoe-Hey@users.noreply.github.com>
2024-10-01 21:25:45 +0200

    create claimservice class to perform functions by adding methods for claim actions

---
+++ POE_Claim_System/Services/ClaimService.cs
@@ -0,0 +1,29 @@
+﻿namespace POE_Claim_System.Services
+{
+    public class ClaimService
+    {
+        public ClaimService() { }
+
+        public int AddNewClaim(ClaimService claim)
+        {
+            //logic to add to claim to db
+
+            return claim.Id;
+        }
+
+        public int UpdateClaim(Claim claim)
+        {
+            //logic to uodate claim to db
+
+            return claim.Id;
+        }
+
+        public List<ClaimService> GetAllClaimsForUser(int personId)
+        {
+            //search on db and return user claims
+
+            return new List<ClaimService>();
+        }
+
+    }
+}
