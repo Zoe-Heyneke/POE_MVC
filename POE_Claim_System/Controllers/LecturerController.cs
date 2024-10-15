dca07c378c5107dbb9ac458d49de4e199ecc9a51
Zoe-Hey <150931346+Zoe-Hey@users.noreply.github.com>
2024-10-15 09:01:38 +0200

    add lecturer controller to get the profile that is logged in

---
+++ POE_Claim_System/Controllers/LecturerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using POE_Claim_System.Services;
+using POE_Claim_System.Services;
+
+namespace POE_Claim_System.Controllers
+{
+    public class LecturerController : Controller
+    {
+        public IActionResult Index()
+        {
+            ClaimService claimService = new ClaimService();
+            var claims = claimService.GetAllClaimsForUser(1);//get the profile that is logged in
+            return View(claims);
+        }
+    }
+}
