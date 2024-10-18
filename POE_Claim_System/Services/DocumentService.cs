af191011bdcad0ab8d900d26bca5ca3a6006e9e6
Zoe-Hey <150931346+Zoe-Hey@users.noreply.github.com>
2024-10-18 07:27:06 +0200

    in this commit the document services was added

--- POE_Claim_System/Services/DocumentService.cs
+++ POE_Claim_System/Services/DocumentService.cs
@@ -1,9 +1,38 @@
-﻿namespace POE_Claim_System.Services
+﻿using POE_Claim_System.Models;
+
+namespace POE_Claim_System.Services
 {
     public class DocumentService
     {
-        //add document
+        ClaimsContext _claimsContext;
+        public DocumentService(ClaimsContext claimsContext)
+        {
+            _claimsContext = claimsContext;
+            _claimsContext.Database.EnsureCreated();
+        }
 
-        //view document
+        //add new document
+        public int AddClaimDocument(Document claimDocument)
+        {
+            _claimsContext.Documents.Add(claimDocument);
+            _claimsContext.SaveChanges();
+            return claimDocument.Id;
+        }
+
+        public void DeleteClaimDocument(int claimDocumentId)
+        {
+            var claimDocument = _claimsContext.Documents.FirstOrDefault(x => x.Id == claimDocumentId);
+            if (claimDocument != null)
+            {
+                _claimsContext.Documents.Remove(claimDocument);
+                _claimsContext.SaveChanges();
+            }
+        }
+        //get documents for a claim
+        public List<Document> GetClaimDocuments(int claimId)
+        {
+            var claimDocuments = _claimsContext.Documents.Where(x => x.ClaimId == claimId).ToList();
+            return claimDocuments;
+        }
     }
 }
