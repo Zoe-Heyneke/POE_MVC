a17d0fbd6e3d30e27638f5111dfc099d705ca440
Zoe-Hey <150931346+Zoe-Hey@users.noreply.github.com>
2024-11-18 14:47:17 +0200

    in this commit file preparation made to allow lecturer to export claims to pdf

--- POE_Claim_System/Controllers/LecturerController.cs
+++ POE_Claim_System/Controllers/LecturerController.cs
@@ -3,6 +3,14 @@ using Microsoft.AspNetCore.Authorization; // Required for [Authorize]
 using POE_Claim_System.Services;
 using POE_Claim_System.Models;
 using Microsoft.AspNetCore.Mvc.Rendering; // Your service that handles claims
+//pdf
+using Microsoft.EntityFrameworkCore.Storage.Json;
+using Newtonsoft.Json;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.Reflection.PortableExecutable;
+using static iTextSharp.text.pdf.AcroFields;
+
 
 namespace POE_Claim_System.Controllers
 {
@@ -46,7 +54,144 @@ namespace POE_Claim_System.Controllers
 
             //fetch claims for lecturer using the valid username
             var claims = _claimService.GetAllClaimsForLecturer(userName);
+            //set convert claims to string
+            HttpContext.Session.SetString("ClaimsData", JsonConvert.SerializeObject(claims));
             return View(claims);
+        }
+
+        public IActionResult DownloadReport()
+        {
+            string fileName = DateTime.Now.ToFileTime() + ".pdf";
+            //get data from session
+            var claimsData = HttpContext.Session.GetString("ClaimsData");
+            var claims = JsonConvert.DeserializeObject<List<ClaimViewModel>>(claimsData!);
+            //now have a list of all claims
+
+            //prepare file
+            iTextSharp.text.Document document = new iTextSharp.text.Document(PageSize.A4, 30, 30, 30, 40);
+            //pdf saved in uploads folder
+            string uploadPath = _uploadFolderPath;
+            var fullPath = Path.Combine(uploadPath, fileName);
+            //create file on system
+            var fs = new FileStream(fullPath, FileMode.Create);
+            PdfWriter writer = PdfWriter.GetInstance(document, fs);
+            Font header = FontFactory.GetFont("Serif", 10, Font.BOLD, BaseColor.BLACK);
+            Font data = FontFactory.GetFont("Serif", 8, Font.NORMAL, BaseColor.BLACK);
+            document.Open();
+            //add information in header
+            document.Add(new Paragraph("Claims Report", header));
+            document.Add(new Paragraph($"Date {DateTime.Now.ToString("dd MMM yyyy HH:mm")}", header));
+            document.Add(new Paragraph($"Please see below a table view of all your claims:", header));
+
+            //set columns and width
+            PdfPTable table = new PdfPTable(8);
+            table.HorizontalAlignment = 0;
+            table.WidthPercentage = 100;
+            float[] widths = new float[] { 20, 20, 10, 10, 10, 10, 10, 10 };
+            table.SetWidths(widths);
+            PdfPCell cell = new PdfPCell();
+            //course column
+            cell.Phrase = new Phrase("Course Code", header);
+            table.AddCell(cell);
+
+            //class column
+            cell = new PdfPCell();
+            cell.Phrase = new Phrase("Class Name", header);
+            table.AddCell(cell);
+
+            //group column
+            cell = new PdfPCell();
+            cell.Phrase = new Phrase("Group", header);
+            table.AddCell(cell);
+
+
+            //date column
+            cell = new PdfPCell();
+            cell.Phrase = new Phrase("Date Claimed", header);
+            table.AddCell(cell);
+
+
+            //hours column
+            cell = new PdfPCell();
+            cell.Phrase = new Phrase("Hours", header);
+            table.AddCell(cell);
+
+
+            //rate column
+            cell = new PdfPCell();
+            cell.Phrase = new Phrase("Rate", header);
+            table.AddCell(cell);
+
+
+            //total column
+            cell = new PdfPCell();
+            cell.Phrase = new Phrase("Total", header);
+            table.AddCell(cell);
+
+
+            //status column
+            cell = new PdfPCell();
+            cell.Phrase = new Phrase("Status", header);
+            table.AddCell(cell);
+
+            //looking through data submitted in claim system
+            foreach (var claim in claims!)
+            {
+                cell = new PdfPCell();
+                cell.Phrase = new Phrase(claim.CourseCode, data);
+                table.AddCell(cell);
+
+                cell = new PdfPCell();
+                cell.Phrase = new Phrase(claim.CourseName, data);
+                table.AddCell(cell);
+
+                cell = new PdfPCell();
+                cell.Phrase = new Phrase(claim.ClassName, data);
+                table.AddCell(cell);
+
+                cell = new PdfPCell();
+                cell.Phrase = new Phrase(claim.DateClaimed.ToString("dd MMM yyyy"), data);
+                table.AddCell(cell);
+
+                cell = new PdfPCell();
+                cell.Phrase = new Phrase(claim.TotalHours.ToString(), data);
+                table.AddCell(cell);
+                cell = new PdfPCell();
+                cell.Phrase = new Phrase(claim.Rate.ToString(), data);
+                table.AddCell(cell);
+
+                cell = new PdfPCell();
+                cell.Phrase = new Phrase(claim.TotalFee.ToString(), data);
+                table.AddCell(cell);
+
+                cell = new PdfPCell();
+                cell.Phrase = new Phrase(claim.Status, data);
+                table.AddCell(cell);
+            }
+
+            //add new cell to show total
+            cell = new PdfPCell();
+            cell.Colspan = 6;
+            cell.Phrase = new Phrase("TOTAL AMOUNT FOR CLAIMS", header);
+            table.AddCell(cell);
+
+            //sum up all claims
+            cell = new PdfPCell();
+            cell.Colspan = 2;
+            cell.Phrase = new Phrase(claims.Sum(x => x.TotalFee).ToString(), header);
+            table.AddCell(cell);
+
+            //format
+            document.Add(new Paragraph(Environment.NewLine));
+            //add table in document
+            document.Add(table);
+
+            //close files to avoid error
+            document.Close();
+            writer.Close();
+            fs.Close();
+
+            return Redirect($"/uploads/{fileName}");
         }
 
 
---
+++ POE_Claim_System/Models/ClaimItemModel.cs
@@ -0,0 +1,16 @@
+﻿namespace POE_Claim_System.Models
+{
+    public class ClaimItemModel
+    {
+        public int Id { get; set; }
+        public DateTime DateClaimed { get; set; }
+        public string PersonName { get; set; }
+        public int StatusId { get; set; }
+        public string Status { get; set; }
+        public string CourseName { get; set; }
+        public string ClassName { get; set; }
+        public int TotalHours { get; set; }
+        public double HourlyRate { get; set; }
+        public double TotalFee { get; set; }
+    }
+}
