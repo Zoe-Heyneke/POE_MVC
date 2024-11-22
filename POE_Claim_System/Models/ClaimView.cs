20b4da852d1c474c3443bb7a47bc3edf584d9cb4
Zoe-Hey <150931346+Zoe-Hey@users.noreply.github.com>
2024-11-22 13:52:16 +0200

    in this commit validation is done in submit claim to allow accurate claim submission

--- POE_Claim_System/Models/ClaimView.cs
+++ POE_Claim_System/Models/ClaimView.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using POE_Claim_System.Models;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace POE_Claim_System.Models
@@ -7,11 +8,20 @@ namespace POE_Claim_System.Models
     public class ClaimView
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Course is required.")]
         public int CourseId { get; set; }
+
+        [Required(ErrorMessage = "Total hours worked is required.")]
+        [Range(1, 60, ErrorMessage = "Total hours must be between 1 and 60.")]
         public int TotalHours { get; set; }
+
+        [Required(ErrorMessage = "Group number is required.")]
         public int ClassId { get; set; }
 
+        [Required(ErrorMessage = "Notes is required.")]
         public string AdditionalNotes { get; set; }
+
+        [Required(ErrorMessage = "Document is required.")]
         public IFormFile Document { get; set; }
         public SelectList? Courses { get; set; }
 
